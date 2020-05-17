2ed804f185a7e5dd7150c25c907ee4cbb37af44b
KharnaievaYuliia <juliiasekret@gmail.com>
2020-05-17 15:22:35 +0300

    ended time show with state

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './App.css';
-
+import Cw2 from "./Components/cw2"
 const App = ( {name} ) => {
 return(
   <div>
 <h1>Hello world!!!</h1>
 <p>Your name {name} </p>
+<Cw2 date={ new Date() }/>
   </div>
   
-)
+);
 }
 export default App
---
+++ src/Components/cw2/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Cw2 = ( {date} )=>{
+    const [time,setTime] = React.useState(date);
+    setInterval(()=>{
+    setTime( new Date() );
+    },1000);
+
+return(
+    <p>{time.toString()}</p>
+)
+}
+export default Cw2
