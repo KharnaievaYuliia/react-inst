965a16f1583c3f6fe539800cea5b317b15457086
KharnaievaYuliia <juliiasekret@gmail.com>
2020-05-17 13:57:49 +0300

    added props

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
 
-const App = () => {
+const App = ( {name} ) => {
 return(
   <div>
 <h1>Hello world!</h1>
+<p>Your name {name} </p>
   </div>
   
 )
