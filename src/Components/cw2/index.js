ced716726c532efd5f43f062d05c2be4c653efa5
KharnaievaYuliia <juliiasekret@gmail.com>
2020-05-17 17:55:17 +0300

    add changes

--- src/Components/cw2/index.js
+++ src/Components/cw2/index.js
@@ -1,13 +1,30 @@
 import React from "react";
+import ClubInfo from "./info";
+import Achievement from "./achievement";
+import Player from "./player";
 
-const Cw2 = ( {date} )=>{
-    const [time,setTime] = React.useState(date);
-    setInterval(()=>{
-    setTime( new Date() );
-    },1000);
-
+const Cw2 = ( {clubData} )=>{
+   const {info,achievements,players} = clubData;
 return(
-    <p>{time.toString()}</p>
-)
+    <>
+    <h2>Club Info</h2>
+    <ClubInfo info={info} />
+    <h2>Achivements</h2>
+   {
+   achievements.map( (item,index)=>{
+   return <Achievement key={index} achievement={item} />
+   })
 }
+<table>
+<tbody>
+{
+   players.map((item,index)=>{
+      return <Player playerInfo={item} key={index} />
+   })
+}
+</tbody>
+</table>
+</>
+);
+};
 export default Cw2
