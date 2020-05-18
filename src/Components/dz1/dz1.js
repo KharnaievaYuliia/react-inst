0e983d7fca5bda3c8aab07a3534e579be4384e7f
KharnaievaYuliia <juliiasekret@gmail.com>
2020-05-18 22:53:48 +0300

    add changes

--- src/Components/dz1/dz1.js
+++ src/Components/dz1/dz1.js
@@ -5,9 +5,9 @@ const Dz1 = () => {
     <div className="App">
     <header className="App-header">
     <h1>My country is Ukraine</h1>
-    <img src={photo} className="App-photo" alt="photo" />
+    <img src="./dz1/photo.jpg" className="App-photo" alt="photo" />
     <h2>My town is Cherkassy!</h2>
-    <img src={logo} className="App-logo" alt="logo" />
+    <img src="./dz1/logo.png" className="App-logo" alt="logo" />
     <p>The quantity of Cherkassy residens is 300000 persons</p>
     </header>
   </div>
