520114e596fa49745dc182a2dc65a088bfffbc4b
KharnaievaYuliia <juliiasekret@gmail.com>
2020-05-18 21:47:09 +0300

    add changes

---
+++ src/Components/dz1/dz1.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './dz1.css';
+
+const Dz1 = () => {
+    <div className="App">
+    <header className="App-header">
+    <h1>My country is Ukraine</h1>
+    <img src={photo} className="App-photo" alt="photo" />
+    <h2>My town is Cherkassy!</h2>
+    <img src={logo} className="App-logo" alt="logo" />
+    <p>The quantity of Cherkassy residens is 300000 persons</p>
+    </header>
+  </div>
+}
+
+export default Dz1;
