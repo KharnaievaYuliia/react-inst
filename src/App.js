5de7631c444043d6ae08750a75c58c79579b70e3
KharnaievaYuliia <juliiasekret@gmail.com>
2020-05-29 21:38:25 +0300

    change

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import React,{useEffect} from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
-import {getAlbumId,getAlbumImages} from "./utils/API";
+import { getAlbumId, getAlbumImages } from "./utils/API";
+import {BrowserRouter as Router, Switch,Route,Link} from "react-router-dom";
 
-const App = (  ) => {
-  const [ albumIdArray,setAlbumIdArray] = React.useState();
- const [albumWithImagesArray,setAlbumWithImagesArray]
-useEffect(()=>{
-getAlbumId()
-.then((res)=>setAlbumIdArray(res.data.data));
-},[]);
-useEffect(()=>{
-let images = [];
+const App = () => {
+  const [albumIdArray, setAlbumIdArray] = React.useState();
+  const [albumWithImagesArray, setAlbumWithImagesArray] = React.useState();
 
-},[albumIdArray]);
-return(
-  <div>
+  useEffect(() => {
+    getAlbumId()
+      .then((res) => setAlbumIdArray(res.data.data));
+  }, []);
+  useEffect(() => {
+    let tmpAlbumIdWithImages = [];
+    albumIdArray && albumIdArray.map((hash) => {
+      getAlbumImages(hash)
+        .then(res => tmpAlbumIdWithImages.push(
+          {
+            id: hash,
+            images: res.data.data,
+          }
+        )
+        );
+    });
+    albumIdArray && setAlbumWithImagesArray(tmpAlbumIdWithImages);
+    console.log(tmpAlbumIdWithImages);
+  }, [albumIdArray]);
 
-  </div>
-  
-)
+  return (
+    <div>
+<Router> {albumWithImagesArray && albumWithImagesArray.map((album)=>{
+  return <Link to={`/${album}`}>{album}</Link>
+})}
+</Router>
+    </div>
+
+  )
 }
 export default App
