6c21feb2de608b0dc7c538836f64fdf3215ecef2
KharnaievaYuliia <juliiasekret@gmail.com>
2020-05-30 15:09:24 +0300

    change

--- src/App.js
+++ src/App.js
@@ -1,40 +1,47 @@
 import React, { useEffect } from 'react';
 import './App.css';
 import { getAlbumId, getAlbumImages } from "./utils/API";
-import {BrowserRouter as Router, Switch,Route,Link} from "react-router-dom";
+import {BrowserRouter as Router, Link} from "react-router-dom";
 
 const App = () => {
   const [albumIdArray, setAlbumIdArray] = React.useState();
-  const [albumWithImagesArray, setAlbumWithImagesArray] = React.useState();
+  const [albumWithImagesArray, setAlbumWithImagesArray] = React.useState([]);
 
   useEffect(() => {
-    getAlbumId()
-      .then((res) => setAlbumIdArray(res.data.data));
+    getAlbumId().then((res) => setAlbumIdArray(res.data.data));
   }, []);
+
   useEffect(() => {
-    let tmpAlbumIdWithImages = [];
-    albumIdArray && albumIdArray.map((hash) => {
-      getAlbumImages(hash)
-        .then(res => tmpAlbumIdWithImages.push(
-          {
-            id: hash,
-            images: res.data.data,
-          }
-        )
-        );
-    });
-    albumIdArray && setAlbumWithImagesArray(tmpAlbumIdWithImages);
-    console.log(tmpAlbumIdWithImages);
+    albumIdArray &&
+      albumIdArray.map((hash) => {
+        getAlbumImages(hash).then((res) => {
+          const result = res.data.data;
+          console.log("result", result);
+          setAlbumWithImagesArray((prev) => [
+            ...prev,
+            { id: hash, images: { ...result } },
+          ]);
+        });
+      });
   }, [albumIdArray]);
 
+  console.log(albumIdArray);
+  console.log(albumWithImagesArray);
   return (
     <div>
-<Router> {albumWithImagesArray && albumWithImagesArray.map((album)=>{
-  return <Link to={`/${album}`}>{album}</Link>
-})}
-</Router>
+      <Router>
+        {albumWithImagesArray &&
+          albumWithImagesArray.length >= 1 &&
+          albumWithImagesArray.map((album) => {
+            return (
+              <div>
+                <Link to={`/${album.id}`}>{album.id}</Link>
+              </div>
+            );
+          })}
+      </Router>
     </div>
+  );
+};
 
-  )
-}
 export default App
