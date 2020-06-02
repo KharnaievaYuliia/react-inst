0f499bfccf836cd3ec89b3fd21cf032322ef347d
KharnaievaYuliia <juliiasekret@gmail.com>
2020-06-02 18:06:13 +0300

    add task

--- src/App.js
+++ src/App.js
@@ -1,48 +1,108 @@
 import React, { useState, useEffect } from 'react';
-import './App.css';
-import { getAlbumId, getAlbumImages } from './utils/API.js';
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import { makeStyles, CssBaseline, Grid } from '@material-ui/core';
+import { getAlbumId, getAlbumImages } from './utils/API.js';
+
+const useStyles = makeStyles({
+  root: {
+    width: '1300px',
+    margin: '0 auto',
+    fontSize: '50px',
+    fontWeight: 'bold',
+    backgroundColor: 'SkyBlue',
+
+  },
+  header: {
+    width: '1300px',
+    backgroundColor: 'SteelBlue',
+    marginBottom: '30px',
+  },
+  link: {
+    padding: '45px',
+    textDecoration: 'none',
+    color: 'DarkSlateGray',
+
+  },
+  img: {
+    overflow: 'hidden',
+    margin: '15px 50px',
+    width: '100%',
+    height: 'auto',
+    borderRadius: '5px',
+    border: '5px solid white',
+  }
+
+});
 
 const App = () => {
-  const [albumIdArray, setAlbumIdArray] = React.useState();
-  const [albumWithImagesArray, setAlbumWithImagesArray] = React.useState([]);
+  const [albumIdArray, setAlbumIdArray] = useState([]);
+  const [albumImages, setAlbumImages] = useState([]);
 
   useEffect(() => {
-    getAlbumId().then((res) => setAlbumIdArray(res.data.data));
+    getAlbumId()
+      .then(res => {
+        const tmpArray = res.data.data.map(item => { return { id: item, title: "" }; });
+        setAlbumIdArray(tmpArray);
+      });
   }, []);
 
   useEffect(() => {
-    albumIdArray &&
-      albumIdArray.map((hash) => {
-        getAlbumImages(hash).then((res) => {
-          const result = res.data.data;
-          console.log("result", result);
-          setAlbumWithImagesArray((prev) => [
-            ...prev,
-            { id: hash, images: { ...result } },
-          ]);
-        });
+    if (albumIdArray) {
+      let tmpAlbumImages = [];
+      let i = 0;
+      albumIdArray.map(album => {
+        return getAlbumImages(album.id)
+          .then(res => {
+            album.title = res.data.data.title;
+            tmpAlbumImages.push(
+              {
+                id: album.id,
+                title: res.data.data.title,
+                images: res.data.data.images
+              }
+            );
+            i++;
+            if (i === albumIdArray.length) setAlbumImages(tmpAlbumImages);
+          })
+
       });
+    }
   }, [albumIdArray]);
 
-  console.log(albumIdArray);
-  console.log(albumWithImagesArray);
+  const classes = useStyles();
+
   return (
-    <div>
-      <Router>
-        {albumWithImagesArray &&
-          albumWithImagesArray.length >= 1 &&
-          albumWithImagesArray.map((album) => {
-            return (
-              <div>
-                <Link to={`/${album.id}`}>{album.id}</Link>
-              </div>
-            );
-          })}
-      </Router>
+
+    <div className={classes.root}>
+      <CssBaseline />
+      <Grid container>
+        <Router>
+          <header className={classes.header}>
+            <Grid item >
+              {albumIdArray.map(album =>
+                <Link className={classes.link} key={album.id} to={`/${album.id}`}>{album.title}</Link>
+              )}
+            </Grid>
+          </header>
+          <Switch>
+            {albumImages.map(album => {
+              return (
+                <Route key={album.id} path={`/${album.id}`}>
+                  <Grid xs={5} md={8} xl={11}>
+                    {album.images.map((img =>
+                      <div key={img.id}>
+                        <img className={classes.img} src={img.link} alt={img.id} />
+                      </div>
+                    ))}
+                  </Grid>
+                </Route>
+              )
+            })}
+          </Switch>
+        </Router>
+      </Grid>
     </div>
   );
-};
+}
 
 export default App;
--- src/utils/API.js
+++ src/utils/API.js
@@ -1,15 +1,18 @@
 import axios from "axios";
 
-const API = "https://api.imgur.com/3";
+const API = "https://api.imgur.com/3/";
 
 const PARAMS={
     headers: {Authorization: "Client-ID dc5a29116fff477"},
 };
 
-const getAlbumId = ()=>{
-   return axios.get(`${API}/account/juliiasekret/albums/ids/0`,PARAMS);
+const getAlbumId = ()=> {
+    return axios.get(`${API}/account/juliiasekret/albums/ids/0`,PARAMS);
 }
-const getAlbumImages = (hash)=>{
-    return axios.get(`${API}/album/${hash}/images`,PARAMS);
-};
-export{getAlbumId,getAlbumImages};
+
+const getAlbumImages = (hash)=> {
+    return axios.get(`${API}/account/juliiasekret/album/${hash}`, PARAMS);
+}
+
+
+export {getAlbumId, getAlbumImages};
